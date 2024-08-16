a4267e49bb486cbf3a1e474b6d36e3a8d0ddd0c9
alaodd <159951731+alaodd@users.noreply.github.com>
2024-08-16 16:15:58 -0700

    added env

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
+require('dotenv').config();
 import App from "@/components/App";
+import { datadogRum } from '@datadog/browser-rum';
+
+datadogRum.init({
+    applicationId: process.env.APPLICATION_ID ?? '',
+    clientToken: process.env.CLIENT_TOKEN ?? '',
+    site: 'datadoghq.com',
+    service: 'react-boilerplate',
+    env: 'staging',
+    sessionSampleRate: 100,
+    sessionReplaySampleRate: 20,
+    trackUserInteractions: true,
+    trackResources: true,
+    trackLongTasks: true,
+    defaultPrivacyLevel: 'mask-user-input',
+});
+
 
 const container = document.getElementById("app");
 if (!container) throw new Error("Failed to find the root element");
